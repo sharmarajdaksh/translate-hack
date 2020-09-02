1870d76e15bbb2cbba803d90296fd636af764dcb
Dakshraj Sharma <sharmarajdaksh@gmail.com>
2020-09-02 21:30:12 +0530

    Users can now input config file path as an argument

--- src/cmd/readInputArguments.js
+++ src/cmd/readInputArguments.js
@@ -1,8 +1,10 @@
 const path = require("path");
+const fs = require("fs-extra");
 
 const argv = require("yargs")
-  .string(["input-file", "output-dir", "output-languages"])
+  .string(["input-file", "output-dir", "output-languages", "config"])
   .example("$0 -i English.json -o locales/ -l Spanish -l French")
+  .example("$0 -f translate-config.json")
   .alias("i", "input-file")
   .describe("i", "Path to input file")
   .nargs("i", 1)
@@ -11,14 +13,38 @@ const argv = require("yargs")
   .nargs("i", 1)
   .alias("l", "output-languages")
   .describe("l", "Language to translate to")
-  .demandOption(["i", "o", "l"])
+  .alias("f", "config")
+  .nargs("f", 1)
+  .describe("f", "Path to config file")
   .help("h")
   .alias("h", "help").argv;
 
 const readInputArguments = () => {
-  const inputFile = path.resolve(argv.i);
-  const outputDir = path.resolve(argv.o);
-  let outputLanguages = argv.l;
+  let inputFile, outputDir, outputLanguages;
+
+  if (argv.f) {
+    const filepath = path.resolve(argv.f);
+    const buffer_data = fs.readFileSync(filePath);
+    let configData = JSON.parse(buffer_data);
+    inputFile = configData.inputFile;
+    outputDir = configData.outputDir;
+    outputLanguages = configData.outputLanguages;
+  }
+  if (argv.i) {
+    inputFile = path.resolve(argv.i);
+  }
+  if (argv.o) {
+    outputDir = path.resolve(argv.o);
+  }
+  if (argv.l) {
+    outputLanguages = argv.l;
+  }
+
+  if (!inputFile || !outputDir || !outputLanguages) {
+    throw Error(
+      "Invalid config. Please check config file and/or command line arguments"
+    );
+  }
 
   if (typeof outputLanguages === "string") {
     outputLanguages = [outputLanguages];
